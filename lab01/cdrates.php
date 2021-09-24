83d585042959f58627949f73515dd022dd800bcf
stavreskimario <59723836+stavreskimario@users.noreply.github.com>
2021-09-24 12:55:29 +1000

    Create cdrates.php

---
+++ lab01/cdrates.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task 3</title>
+</head>
+<body>
+    <h1>Forestville Credit Union</h1>
+    <h2>CD Rates</h2>
+    
+    <?php
+        
+        echo "<ul>
+            <li> 4.35% (36-Month Term CD) </li> <br/>
+            <li> 3.85% (12-Month Term CD) </li> <br/>
+            <li> 2.65% (6-Month Term CD) </li>
+        </ul>";
+
+        
+        echo "$1,000 minimum deposit";
+    // Mario Stavreski; 103055993 - 4 August 2021
+    ?>
+
+</body>
+</html>

ca74fe090fc83565534b979fa206a79497b06ce4
stavreskimario <59723836+stavreskimario@users.noreply.github.com>
2021-09-24 12:55:54 +1000

    Create primenumberform.php

---
+++ lab03/primenumberform.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Lab 3</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Advanced Web Development">
+    <meta name="keywords" content="HTML, CSS, JavaScript, PHP">
+    <meta name="author" content="Mario Stavreski">
+</head>
+<body>
+    <h1>Lab 3 - Task 3</h1>
+    <form action="primenumber.php" method="GET">
+        <label for="number">Number:</label>
+        <input type="number" id="number" name="number">
+        <button type="submit" value="submit">Check for Prime Number</button>
+    </form>
+
+    
+</body>
+</html>

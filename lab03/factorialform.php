4b729b0bc90a1ecf27c16651b86b2097c941ce66
stavreskimario <59723836+stavreskimario@users.noreply.github.com>
2021-09-24 12:55:44 +1000

    Create factorialform.php

---
+++ lab03/factorialform.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 3</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Advanced Web Development">
+    <meta name="keywords" content="HTML, CSS, JavaScript, PHP">
+    <meta name="author" content="Mario Stavreski">
+</head>
+<body>
+    <h1>Web Programming - Lab 3</h1>
+    <form action="factorial.php" method="GET">
+        <label for="number">Number:</label>
+        <input type="number" id="number" name="number">
+        <button type="submit" value="submit">Enter</button>
+    </form>
+
+    
+</body>
+</html>

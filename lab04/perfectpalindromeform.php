fb3c53a5eebea5eafd877a981decf9ac996f4cf2
stavreskimario <59723836+stavreskimario@users.noreply.github.com>
2021-09-24 12:55:58 +1000

    Create perfectpalindromeform.php

---
+++ lab04/perfectpalindromeform.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 4</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Web development">
+    <meta name="keywords" content="HTML, PHP">
+    <meta name="author" content="Mario Stavreski">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Web Programming - Lab 4</h1>
+    <form action="perfectpalindrome.php" method="POST">
+        <div class="form-group">
+            <label for="string">String:</label>
+            <input class="form-control" type="string" id="string" name="string">
+            <button class="btn btn-primary" type="submit" value="submit">Check for Palindrome</button>
+        </div>
+    </form>
+
+    
+</body>
+</html>

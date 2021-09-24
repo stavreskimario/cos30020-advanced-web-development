67253d00fe9c07e650e15ced79915ab731aa4df5
stavreskimario <59723836+stavreskimario@users.noreply.github.com>
2021-09-24 12:55:48 +1000

    Create leapyearform.php

---
+++ lab03/leapyearform.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Lab 3</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Advanced Web Development">
+    <meta name="keywords" content="HTML, CSS, JavaScript, PHP">
+    <meta name="author" content="Mario Stavreski">
+</head>
+<body>
+    <h1>Lab 3 - Task 2</h1>
+    <form action="leapyear.php" method="GET">
+        <label for="year">Year:</label>
+        <input type="year" id="year" name="year">
+        <button type="submit" value="submit">Check for Leap Year</button>
+    </form>
+
+    
+</body>
+</html>

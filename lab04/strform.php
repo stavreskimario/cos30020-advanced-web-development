731c1b552de0bd715b82e813f6580044b42dbbdc
stavreskimario <59723836+stavreskimario@users.noreply.github.com>
2021-09-24 12:56:01 +1000

    Create strform.php

---
+++ lab04/strform.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 4</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Web development">
+    <meta name="keywords" content="HTML, PHP">
+    <meta name="author" content="Mario Stavreski">
+</head>
+<body>
+    <h1>Web Programming - Lab 4</h1>
+    <form action="strprocess.php" method="POST">
+        <label for="string">String:</label>
+        <input type="string" id="string" name="string">
+        <button type="submit" value="submit">Enter</button>
+    </form>
+
+    
+</body>
+</html>
